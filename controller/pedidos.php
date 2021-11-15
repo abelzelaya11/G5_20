b0044a7d98dfe557ce15d9fc11d26d75bdf28fae
abelzelaya11 <abelenriquezelaya@hotmail.com>
2021-11-15 12:12:56 -0600

    agregar metodos

--- controller/pedidos.php
+++ controller/pedidos.php
@@ -1,11 +1,21 @@
 <?php
-    header ('Content-Type: application/json');
+    if($_SERVER['REQUEST_METHOD'] === 'OPTIONS'){
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+        header('Access-Control-Allow-Headers: token, Content-Type');
+        header('Access-Control-Max-Age: 1728000');
+        header('Control-Length: 0');
+        header('Content Type: text/plain');
+        die();
+    }
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
 
     require_once("../config/conexion.php");
     require_once("../models/Pedidos.php");
     $pedidos = new Pedidos();
 
-    $body = json_decode(file_get_contents("pho://input"), true);
+    $body = json_decode(file_get_contents("php://input"), true);
 
     switch($_GET["op"]){
 
